011bba5b6aa410dcbd48a22edc357b5890b6c0db
-Paniz- <99718844+paniz-mohammadi@users.noreply.github.com>
2025-11-20 12:51:47 +0100

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,11 +1,9 @@
-import React, {useEffect} from 'react';
+import React from 'react';
 import {BrowserRouter, Routes, Route} from 'react-router-dom';
 import {FiSettings} from 'react-icons/fi';
 import {TooltipComponent} from '@syncfusion/ej2-react-popups';
 import {Navbar, Footer, Sidebar, ThemaSettings} from './components';
 import {Ecommerce, Orders, Calendar, Employees, Stacked, Pyramid, Customers, Kanban, Line, Area, Bar, Pie, Financial, ColorPicker, ColorMapping, Editor} from './pages';
-//import {useStateContext} from './contexts/ContextProvider';
-//import{Navbar} from 'react-bootstrap';
 import {useStateContext} from './contexts/ContextProvider';
 import './App.css';
 
